6f0c2ef477b67401e6ee740706ab0a2258f58904
Akash Chowdhury <coderboyakash@Akashs-iMac-Pro.local>
2020-08-04 17:36:55 +0530

    display data using GET

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello World!!</h1>")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}")
