bf8c78bca08272c8322c14a094f7a099663474bf
Akash Chowdhury <coderboyakash@Akashs-iMac-Pro.local>
2020-08-04 22:55:42 +0530

    new app task

--- CS50/urls.py
+++ CS50/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myfirstapp/', include("myfirstapp.urls")),
+    path('tasks/', include("tasks.urls")),
 ]
